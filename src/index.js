683a019d9172b71a85bcc4a75d2c9c13ef7436b4
CTC <CTC@dc.liftservis.by>
2020-03-18 16:52:50 +0300

    feah: add of  function brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let bracket = bracketsConfig[i].join('');
+    if (str.includes(bracket)) {
+        str = str.replace(bracket, '');
+        i = -1;
+    } 
+  }
+  let res = str.length ? false : true;
+  return res;
 }
